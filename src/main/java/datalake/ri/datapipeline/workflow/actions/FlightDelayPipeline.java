27d9d43086e851e29af521758d01dcd5b76f4afa
Abhishek Gupta <abhishkg@thoughtworks.com>
2016-06-02 15:16:52 +0530

    Customized the user property.Now user name is inside properties file in resources/user directory.

--- src/main/java/datalake/ri/datapipeline/workflow/actions/FlightDelayPipeline.java
+++ src/main/java/datalake/ri/datapipeline/workflow/actions/FlightDelayPipeline.java
@@ -3,6 +3,10 @@ package datalake.ri.datapipeline.workflow.actions;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class FlightDelayPipeline {
     protected static JavaSparkContext javaSparkContext;
     protected static String inputFileName;
@@ -10,14 +14,23 @@ public class FlightDelayPipeline {
     protected static String user;
 
     protected static void initialize(String[] args) {
-        if (args.length != 3) {
+        if (args.length != 2) {
             System.out.println("Invalid Length of Arguments");
             System.exit(1);
         }
-        SparkConf sparkConf = new SparkConf().setAppName("Datalake Pipeline");
+        try {
+            InputStream stream = FlightDelayPipeline.class.getResourceAsStream("/user/user.properties");
+            Properties properties = new Properties();
+            properties.load(stream);
+            user = properties.getProperty("user");
+        } catch (IOException e) {
+            System.out.println("Can not load User File in user/user.properties in your jar");
+            System.exit(1);
+        }
+        System.out.println("Executing the command for the user:--" + user);
+        SparkConf sparkConf = new SparkConf().setAppName("DataLake Pipeline");
         javaSparkContext = new JavaSparkContext(sparkConf);
-        user = args[0];
-        inputFileName = args[1];
-        outputFileName = args[2];
+        inputFileName = args[0];
+        outputFileName = args[1];
     }
 }
