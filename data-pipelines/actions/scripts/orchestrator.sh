af4d2dd55b88478158e9b937f5b15d4f3249d136
Abhishek Gupta <abhishkg@thoughtworks.com>
2017-01-10 19:10:22 +0530

    [Abhishek]-Added an spark-submit and sqoop export to the database commands in the orchestrator.

--- data-pipelines/actions/scripts/orchestrator.sh
+++ data-pipelines/actions/scripts/orchestrator.sh
@@ -7,7 +7,7 @@ sqoop import --connect jdbc:postgresql://localhost:5432/tpch-benchmark\
  --table nation \
  --target-dir  ../tpch-benchmark-datasets/s-factor-1/nation/ \
  --optionally-enclosed-by '\"' \
- -- --schema raw_store
+ -- --schema raw_store;
 
 sqoop import --connect jdbc:postgresql://localhost:5432/tpch-benchmark\
  --username postgres \
@@ -16,6 +16,13 @@ sqoop import --connect jdbc:postgresql://localhost:5432/tpch-benchmark\
  --table region \
  --target-dir  ../tpch-benchmark-datasets/s-factor-1/region/ \
  --optionally-enclosed-by '\"' \
- -- --schema raw_store
+ -- --schema raw_store;
 
 rm nation.java region.java
+
+spark-submit --jars prep-buddy.jar --class NationDimension data-lake.jar "";
+
+sqoop export --connect --connect jdbc:postgresql://localhost:5432/tpch-benchmark \
+ --username postgres --password ${PASSWORD} --table nation_dim \
+ --export-dir deNormalizedNationTable \
+ --optionally-enclosed-by '\"' -m 1 -- --schema tpch_star_schema;
